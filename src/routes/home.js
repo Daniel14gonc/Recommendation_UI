f72ddc980888120d663dc84022baae370618566d
Daniel14gonc <71653466+Daniel14gonc@users.noreply.github.com>
2022-03-31 15:41:26 -0600

    Carrousel

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,10 +1,5 @@
 import './home.css'
 
-
-
-
-
-
 const Header = () =>{
 
   return(
@@ -13,7 +8,15 @@ const Header = () =>{
 
       </div>
       <div className='navegable'>
-        
+        <div>
+          <p>Inicio</p>
+        </div>
+        <div>
+          <p>Explorar</p>
+        </div>
+        <div>
+          <p>Mi lista</p>
+        </div>
       </div>
       <div className='lupa'>
 
@@ -25,8 +28,44 @@ const Header = () =>{
   )
 }
 
+const Pelicula = () => {
+  return (
+    <div className='pelicula'>
+
+    </div>
+  )
+}
 
 
+const Carrousel = () => {
+
+  const temp = ['hola', 'adios', 'si', 'no', 'hola', 'adios', 'si', 'no']
+
+  return (
+    <div className='carrousel'>
+      <div style={{color:'white', fontSize:'20px'}}>Nombre</div>
+      <div className='containMovies'>
+        {
+          temp.map(() => {
+            return (<Pelicula />)
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+
+const BigFilm = ({image}) => {
+  return (
+    <div className='superDiv' style={{backgroundImage:`url(${image})`}}>
+      <div className='superFilm'>
+        <div className='play'></div>
+        <div style = {{marginLeft:'30px'}}>Reproducir</div>
+      </div>
+    </div>
+  )
+}
 
 
 
@@ -37,8 +76,10 @@ const Home = () =>{
     <div className="containerhome">
 
       <Header/>
-      <div className='content'>
-        
+      <div className='contentFilms'>
+        <BigFilm />
+
+        <Carrousel />
       </div>
     </div>
   )
