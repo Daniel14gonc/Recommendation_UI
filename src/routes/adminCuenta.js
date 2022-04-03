619f76f50b5db27980b2491543ba600e9e06e5a3
franco611 <62269078+franco611@users.noreply.github.com>
2022-04-03 16:38:23 -0600

    cambios admincuenta
    cambios admincuenta

--- src/routes/adminCuenta.js
+++ src/routes/adminCuenta.js
@@ -12,27 +12,27 @@ const AdminCuenta = () => {
 
     return(
         <div className="containeradCuenta">
-          <div className="regreso" onClick={()=> regresoHome()}/>
+          <div className="headercito1"><div className="regreso" onClick={()=> regresoHome()}/></div>
           <div className='fondo'>
             <div className="adCuenta">
               <p className="texto">{cuenta}</p>
             </div>
-            <div className='accountholder'>
-              <div className='acctype'>
+            <div className='accountholder1'>
+              <div className='acctype1'>
                 <div>
                   <input type = 'radio' id = 'basica' value='basica'  />
                   <label for = 'basica'>Basica</label> 
                 </div>
                 <a>1 perfil</a>
               </div>
-              <div className='acctype'>
+              <div className='acctype1'>
                 <div>
                   <input type = 'radio' id = 'estandar' value='estandar' />
                   <label for = 'estandar'>Estandar</label>
                 </div>
                 <a>4 perfiles</a>
               </div>
-              <div className='acctype'>
+              <div className='acctype1'>
                 <div>
                   <input type = 'radio' id = 'avanzada' value='avanzada'/>
                   <label for = 'acanzada'>Avanzada</label>
